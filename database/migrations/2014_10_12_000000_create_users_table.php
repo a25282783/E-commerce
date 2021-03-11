6e4f003a2f5b1d65315aafa2415c02f788832b94
louis <bobbyliou830228@gmail.com>
2021-03-11 15:31:34 +0800

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->string('first_name')->default('');
+            $table->string('last_name')->default('');
+            $table->string('mobile')->default('');
+            $table->string('address')->default('');
+            $table->string('city')->default('');
+            $table->string('state')->default('');
+            $table->string('zip_code')->default('');
+            $table->string('country')->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
